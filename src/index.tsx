908f51170850c1e736edc84354b8396051344dd1
Quinton Hoffman <quinton.hoffman@asurion.com>
2021-04-14 13:29:04 -0500

    chore: initial commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import { requireNativeComponent, ViewStyle } from 'react-native';
+
+type AccessibilityGroupingProps = {
+  color: string;
+  style: ViewStyle;
+};
+
+export const AccessibilityGroupingViewManager = requireNativeComponent<AccessibilityGroupingProps>(
+'AccessibilityGroupingView'
+);
+
+export default AccessibilityGroupingViewManager;
